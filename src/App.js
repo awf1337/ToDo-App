0ccfd62effb87822ad85ca234cfd482acdeeecf1
awf1337 <101428860+awf1337@users.noreply.github.com>
2022-08-03 14:32:22 +0300

    DryCode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, {useState, useRef} from "react"
 
+// number of items per page
 const PER_PAGE = 5;
 
 export default function App() {
   const [toDoArray, setToDoArray] = useState([]);
   const [page, setPage] = useState(0);
-  const inputRef = useRef(null);
-
   const [inputValue, setInputValue] = useState('');
 
+  const inputRef = useRef(null);
 
-  // adds into array
-  function addIntoArray() {
+  function addIntoArray(e) {
+    //prevent refresh after form submit
+    e.preventDefault()
+
     setToDoArray(prevState => ([inputValue, ...prevState]))
+    //clear the input field after submit
     setInputValue('');
 
-    //clear the input after submit
+    //focus the input field after submit
     inputRef.current.focus()
   }
 
   function deleteItem(indexElement) {
-    //deep copy of ToDo array 
+    //deep copy of toDoArray array 
     const updatedArrayList = JSON.parse(JSON.stringify(toDoArray))
 
     //remove indexElement item from array
@@ -32,9 +35,8 @@ export default function App() {
   const toDoList = toDoArray.map((toDoElement,indexElement) => {
         return (
               <div className="todo_Content" key={indexElement}>
-                {/* <div> for layout */}
-                <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                  <li className="list"> {toDoElement} </li>
+                <div className="todo_Items">
+                  <li> {toDoElement} </li>
                 </div>
                 <span className="todo_DeleteItem" onClick={() => deleteItem(indexElement)}>X</span>
               </div>
@@ -46,24 +48,24 @@ export default function App() {
 
         <div>
           <h1>You have {toDoArray.length} Todos</h1>
-            <ul className="todo_List">
-              {toDoList.slice(page * PER_PAGE, (page + 1) * PER_PAGE)}
-            </ul> 
-          {toDoArray.length > 5 &&
-          <>
-            
-            {[...Array(parseInt(toDoArray.length/5) + 1)].map((_, i) => (
-              <a onClick={() => setPage(i)}>{i+1}</a> 
-            ))}
 
-          </>
+          <ul className="todo_List">
+            {toDoList.slice(page * PER_PAGE, (page + 1) * PER_PAGE)}
+          </ul> 
+
+          {toDoArray.length > 5 &&
+            <div className="todo_Pages">
+              {[...Array(parseInt(toDoArray.length/5) + 1)].map((_ , i) => (
+                <span onClick={() => setPage(i)}>{i+1}</span> 
+              ))}
+            </div>
           }
         </div>
         
-        <div className="todo_Form">
-            <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} ref={inputRef} id="todo_Input" />
+        <form>
+            <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} ref={inputRef}/>
             <button className="todo_Button" onClick={addIntoArray}>Submit</button>
-        </div>
+        </form>
 
       </div>
   )
