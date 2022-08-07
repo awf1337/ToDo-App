c07068eb2c3503aaed83e09151ced128705b7687
awf1337 <101428860+awf1337@users.noreply.github.com>
2022-08-07 23:24:51 +0300

    updateRomeo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from "react"
+import React, { useState, useRef, useEffect } from "react"
 import dndImage from "./images/bars-solid.svg"
 import deleteImage from "./images/trash-can-solid.svg"
 
@@ -10,8 +10,8 @@ export default function App() {
   const [page, setPage] = useState(0);
   const [inputValue, setInputValue] = useState('');
   const [inputEditValue, setInputEditValue] = useState('');
-  const [editField,setEditField] = useState({index:NaN,boolean:false})
-  const [onMouseOutClick,setOnMouseOutClick] = useState(false)
+  const [editField, setEditField] = useState(null)
+  const [onMouseOutClick, setOnMouseOutClick] = useState(false)
 
   const inputRef = useRef(null);
   const dragItem = useRef(null);
@@ -21,7 +21,7 @@ export default function App() {
     let dublicateToDoArray = [...toDoArray];
 
     // remove and save the dragged item
-    const draggedItemContent = dublicateToDoArray.splice(dragItem.current,1)[0];
+    const draggedItemContent = dublicateToDoArray.splice(dragItem.current, 1)[0];
 
     //switch the position
     dublicateToDoArray.splice(dragOverItem.current, 0, draggedItemContent)
@@ -52,124 +52,124 @@ export default function App() {
     const updatedArrayList = [...toDoArray];
 
     //remove indexElement item from array
-    updatedArrayList.splice(indexElement,1);
+    updatedArrayList.splice(indexElement, 1);
     setToDoArray(updatedArrayList);
   }
 
   //when remove last item from a page, focus page-1
-  useEffect(()=> {
-    if(toDoArray.length%PER_PAGE === 0){
-      setPage(prevState => prevState > 0 ? prevState-1 : prevState)
+  useEffect(() => {
+    if (toDoArray.length % PER_PAGE === 0) {
+      setPage(prevState => prevState > 0 ? prevState - 1 : prevState)
     }
-  },[toDoArray])
-  
+  }, [toDoArray])
+
   //open edit field on double click
-  function handleClick (event,indexElement) {
+  function handleClick(event, indexElement) {
     setInputEditValue(toDoArray[indexElement])
     switch (event.detail) {
       case 2:
-        setEditField(prevState => ({boolean:!prevState.boolean,index:indexElement}))
+        setEditField(indexElement)
         break;
     }
   }
 
   //edit element and save it
-  function editItem (e,indexElement) {
+  function editItem(e, indexElement) {
     setInputEditValue(e.target.value)
 
     //save the edited item in ToDoArray at same index
     let dublicateArray = [...toDoArray]
-    dublicateArray.splice(indexElement,1,e.target.value)
+    dublicateArray.splice(indexElement, 1, e.target.value)
     setToDoArray(dublicateArray)
   }
 
   //listen to ENTER to close the edit input field
   function listenToEnter(e) {
     if (e.key === "Enter") {
-      setEditField(prevState => ({index:NaN, boolean:!prevState.boolean}))
+      setEditField(null)
       setInputEditValue("")
     }
   }
 
   //listen to Click outside of field to close the edit input field
   function listenToMouseClick() {
-    if(onMouseOutClick === true){
-      setEditField(prevState => ({index:NaN, boolean:!prevState.boolean}))
+    if (onMouseOutClick === true) {
+      setEditField(null)
       setInputEditValue("")
       setOnMouseOutClick(false)
     }
   }
 
-  const toDoList = toDoArray.map((toDoElement,indexElement) => {
-        return (
-              <div 
-              key={indexElement}
-              className={`todo_LineContent Color${ indexElement % 2 === 0 && "Grey" }`}
-              onDragEnter={() => dragOverItem.current = indexElement}
-              >
-                  <img 
-                  src={dndImage} 
-                  alt="" 
-                  className="dndImage"
-                  draggable
-                  onDragStart={() => dragItem.current = indexElement}
-                  onDragEnd={handleSort}
-                  onDragOver={(e) => e.preventDefault()}
-                  />
+  const toDoList = toDoArray.map((toDoElement, indexElement) => {
+    return (
+      <div
+        key={indexElement}
+        className={`todo_LineContent Color${indexElement % 2 === 0 && "Grey"}`}
+        onDragEnter={() => dragOverItem.current = indexElement}
+      >
+        <img
+          src={dndImage}
+          alt=""
+          className="dndImage"
+          draggable
+          onDragStart={(e) => dragItem.current = indexElement}
+          onDragEnd={handleSort}
+          onDragOver={(e) => e.preventDefault()}
+        />
 
-                  {editField.index === indexElement && editField.boolean === false
-                  ?
-                  <input 
-                  value={inputEditValue} 
-                  onMouseOut={() => setOnMouseOutClick(true)}
-                  onKeyUp={(e) => listenToEnter(e)} 
-                  onChange={(e)=> editItem(e,indexElement)}/> 
-                  :
-                  <p className="toDoElement" onClick={(event) => handleClick(event,indexElement)}> {toDoElement} </p>}
-                  
-                  <img 
-                  onClick={() => deleteItem(indexElement)} 
-                  src={deleteImage}
-                  alt=""
-                  className="deleteImage"
-                  />
-              </div>
-        )
+        {editField === indexElement
+          ?
+          <input
+            value={inputEditValue}
+            onMouseOut={() => setOnMouseOutClick(true)}
+            onKeyUp={(e) => listenToEnter(e)}
+            onChange={(e) => editItem(e, indexElement)} />
+          :
+          <p className="toDoElement" onClick={(event) => handleClick(event, indexElement)}> {toDoElement} </p>}
+
+        <img
+          onClick={() => deleteItem(indexElement)}
+          src={deleteImage}
+          alt=""
+          className="deleteImage"
+        />
+      </div>
+    )
   })
 
   return (
-      <div className="main_Container" onMouseDown={() => listenToMouseClick()}>
+    <div className="main_Container" onMouseDown={() => listenToMouseClick()}>
 
-        <div className="mainContent_Layout">
-          <h1>You have {toDoArray.length} Todos</h1>
+      <div className="mainContent_Layout">
+        <h1>You have {toDoArray.length} Todos</h1>
 
-          <ul className="todo_List">
-            {toDoList.slice(page * PER_PAGE, (page + 1) * PER_PAGE)}
-          </ul> 
+        <ul className="todo_List">
+          {toDoList.slice(page * PER_PAGE, (page + 1) * PER_PAGE)}
+        </ul>
 
-          {toDoArray.length > PER_PAGE &&
-            <div className="todo_Pages">
-              {[...Array(Math.floor(toDoArray.length%PER_PAGE === 0
-              ? (toDoArray.length/PER_PAGE) : (toDoArray.length/PER_PAGE + 1)))].map((_ , i) => (
-                <span key={i} onClick={() => setPage(i)}>{i+1}</span> 
+        {toDoArray.length > PER_PAGE &&
+          <div className="todo_Pages">
+            {[...Array(Math.floor(toDoArray.length % PER_PAGE === 0
+              ? (toDoArray.length / PER_PAGE) : (toDoArray.length / PER_PAGE + 1)))].map((_, i) => (
+                <span key={i} onClick={() => setPage(i)}>{i + 1}</span>
               ))}
-            </div>
-          }
-        </div>
-        
-        <form>
-            <input 
-            value={inputValue} 
-            onChange={(e) => setInputValue(e.target.value)} 
-            placeholder="Double click item to edit"
-            ref={inputRef}
-            />
-            <button 
-            className="todo_Button" 
-            onClick={addIntoArray} 
-            disabled={inputValue && inputValue !== " " ? false : true}>Submit</button>
-        </form>
-
+          </div>
+        }
       </div>
+
+      <form>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Double click item to edit"
+          ref={inputRef}
+        />
+        <button
+          className="todo_Button"
+          onClick={addIntoArray}
+          disabled={inputValue && inputValue.match(/\S/) ? false : true}>Submit</button>
+      </form>
+
+    </div>
   )
 }
